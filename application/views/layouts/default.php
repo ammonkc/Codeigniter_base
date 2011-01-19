6092fd8fa12c29ad67c84f9e11773e9aeccc6f9e
Ammon Casey <ammonkc@gmail.com>
2011-01-18 16:20:39 -1000

    nothing

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -10,7 +10,7 @@
         <?php
             $this->carabiner->display();
             echo '<!--[if IE 6]>' . $this->carabiner->display_string('ie6') . '<![endif]-->';
-            echo '<!--[if IE 7]>' . $this->carabiner->display_string('ie7') . '<![endif]-->'; 
+            echo '<!--[if IE 7]>' . $this->carabiner->display_string('ie7') . '<![endif]-->';
             echo '<!--[if IE 8]>' . $this->carabiner->display_string('ie8') . '<![endif]-->';
             echo $yield_head_block;
         ?>

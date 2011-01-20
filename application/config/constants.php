ad707d9ec1ad7ce957b546d782f5d269746743ee
Ammon Casey <ammonkc@gmail.com>
2011-01-20 10:53:36 -1000

    Added Time and date constants

--- application/config/constants.php
+++ application/config/constants.php
@@ -36,6 +36,47 @@ define('FOPEN_READ_WRITE_CREATE', 				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT', 			'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
+/*
+|--------------------------------------------------------------------------
+| Times
+|--------------------------------------------------------------------------
+|
+| http://jamieonsoftware.com/journal/2011/1/20/syntax-sugar-1-times-and-dates.html
+| 
+| I always define constants for my times and dates in - when using CodeIgniter, 
+| the config/constants.php comes in very handy - which makes working with 
+| timestamps read like plain English. It's also incredibly easy to do. 
+| I define MINUTE to be 60 seconds, and then increment up the time ladder, 
+| and define plurals as it reads better.
+|
+*/
+
+define('MINUTE', 60);
+define('MINUTES', MINUTE);
+define('HOUR', 60*MINUTES);
+define('HOURS', HOUR);
+
+/*
+|--------------------------------------------------------------------------
+| Dates
+|--------------------------------------------------------------------------
+|
+| http://jamieonsoftware.com/journal/2011/1/20/syntax-sugar-1-times-and-dates.html
+|
+*/
+
+define('HALF_DAY', 12*HOURS);
+define('HALF_DAYS', HALF_DAY);
+define('DAY', 2*HALF_DAY);
+define('DAYS', DAY);
+define('WEEK', 7*DAYS);
+define('WEEKS', WEEK);
+define('MONTH', 4*WEEKS);
+define('MONTHS', MONTH);
+define('YEAR', 12*MONTHS);
+define('YEARS', YEAR);
+define('FORTNIGHT', 2*WEEKS);
+define('DECADE', 10*YEARS);
 
 /* End of file constants.php */
 /* Location: ./system/application/config/constants.php */
